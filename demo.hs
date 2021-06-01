b6051fd0ad51c3423968c69214b6b3eaa51c13ad
Tanguy <tanguy.brousseau@gmail.com>
2021-06-02 00:36:34 +0200

    lets go

---
+++ demo.hs
@@ -0,0 +1,11 @@
+-- Ok mon sauce, demo time
+import Data.List
+
+trueAndFasle = True && False
+false = not(True)
+int1 = 67
+
+sayHello = "Hello le sang !"
+
+main = do
+print (sayHello, trueAndFasle, int1)
